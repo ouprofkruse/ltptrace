5bfee509ec345276cca7438da8c6fc74b6069d69
hkruse <hans@profkruse.net>
2023-10-17 22:47:46 -0400

    Changed to plotting in 2nd pass, with vertical data offsets

--- ltptrace.py
+++ ltptrace.py
@@ -1,7 +1,107 @@
 # LTP Trace -- Hans Kruse 2023
+# Segment type list from RFC 5326
+"""
+   CTRL EXC Flag 1 Flag 0 Code  Nature of segment
+   ---- --- ------ ------ ----  ---------------------------------------
+     0   0     0      0     0   Red data, NOT {Checkpoint, EORP or EOB}
+     0   0     0      1     1   Red data, Checkpoint, NOT {EORP or EOB}
+     0   0     1      0     2   Red data, Checkpoint, EORP, NOT EOB
+     0   0     1      1     3   Red data, Checkpoint, EORP, EOB
+
+     0   1     0      0     4   Green data, NOT EOB
+     0   1     0      1     5   Green data, undefined
+     0   1     1      0     6   Green data, undefined
+     0   1     1      1     7   Green data, EOB
+
+     1   0     0      0     8   Report segment
+     1   0     0      1     9   Report-acknowledgment segment
+     1   0     1      0    10   Control segment, undefined
+     1   0     1      1    11   Control segment, undefined
+
+     1   1     0      0    12   Cancel segment from block sender
+     1   1     0      1    13   Cancel-acknowledgment segment
+                                to block sender
+
+     1   1     1      0    14   Cancel segment from block receiver
+     1   1     1      1    15   Cancel-acknowledgment segment
+                                to block receiver
+
+"""
 #import math
 import sys
 import os
+from operator import itemgetter
+
+#status and error messages
+MSGLIST = ["Only sessions to/from the first IP found are analyzed", \
+           "Green data part found - not analyzed", \
+           "Cancel segment found - not analyzed", \
+           "Session starts with a control segment", \
+           "Undefined control segment seen" \
+           ]
+MSGSTATUS = [False, False, False, False, False]
+
+
+class Segment:
+    def __init__(self,datastring):
+#        self.error="ok"
+        items = datastring.split("\t")
+        self.frame=int(items[0])
+        self.time=float(items[1])
+        self.type=int(items[7],0)
+        ips=items[2].split(",")
+        self.ports=items[3].split(",")
+        if self.type < 8 or self.type == 9 or self.type == 12 or self.type == 15:
+            self.dsource=ips[0]
+            self.ddest=ips[1]
+        elif self.type == 8 or self.type == 13 or self.type == 14:
+            self.dsource=ips[1]
+            self.ddest=ips[0]
+        else:
+            MSGSTATUS[4] = True
+
+        self.session=int(items[4])
+
+        if self.type < 8:
+            self.offset=int(items[5])
+            self.length=int(items[6])
+        else:
+            self.offset=-1
+            self.length=-1
+            if self.type == 8 :
+                self.rptCount=int(items[8])
+                temp=items[9].split(",")
+                self.rptOffset = [int(x) for x in temp]
+                temp=items[10].split(",")
+                self.rptLength = [int(x) for x in temp]
+
+class SessionData:
+    def __init__(self,segment):
+        if segment.type >= 8:
+            MSGSTATUS[3] = True
+        self.count=1
+        self.time_min=segment.time
+        self.time_max=self.time_min
+        self.dtime_min=segment.time
+        self.dtime_max=segment.time
+        self.data_start=segment.offset
+        self.data_end=segment.offset+segment.length
+        self.session=segment.session
+        self.segments = [segment]
+    def update(self,segment):
+        self.count+=1
+        self.segments.append(segment)
+        self.time_min=min(segment.time,self.time_min)
+        self.time_max=max(segment.time,self.time_max)
+        if segment.type < 8:
+            self.data_start=min(self.data_start,segment.offset)
+            self.data_end=max(self.data_end,segment.offset+segment.length)
+            self.dtime_min=min(segment.time,self.dtime_min)
+            self.dtime_max=max(segment.time,self.dtime_max)
+
+
+segList = []
+session_list = {}
 if (len(sys.argv) < 2):
     print("Usage: python ltptrace.py <filename>")
     quit()
@@ -15,53 +115,6 @@ while (infilename[1] != ""):
 
 PLOTFILE = infilename[0]+".xpl"
 print("Writing to",PLOTFILE)
-
-class Segment:
-    def __init__(self,datastring):
-#        self.error="ok"
-        items = datastring.split("\t")
-        self.frame=int(items[0])
-        self.time=float(items[1])
-        self.type=int(items[7],0)
-        ips=items[2].split(",")
-        self.ports=items[3].split(",")
-        if self.type < 8 :
-            self.dsource=ips[0]
-            self.ddest=ips[1]
-        else:
-            self.dsource=ips[1]
-            self.ddest=ips[0]
-        self.session=int(items[4])
-        if self.type < 8 :
-            self.offset=int(items[5])
-            self.length=int(items[6])
-        else:
-            self.offset=-1
-            self.length=-1
-            if self.type == 8 :
-                self.rptCount=int(items[8])
-                temp=items[9].split(",")
-                self.rptOffset = [int(x) for x in temp]
-                temp=items[10].split(",")
-                self.rptLength = [int(x) for x in temp]
-class SessionData:
-    def __init__(self,segment):
-        self.count=1
-        self.time_min=segment.time
-        self.time_max=self.time_min
-        self.data_start=segment.offset
-        self.data_end=segment.offset+segment.length
-        self.session=segment.session
-    def update(self,segment):
-        self.count+=1
-        self.time_min=min(segment.time,self.time_min)
-        self.time_max=max(segment.time,self.time_max)
-        self.data_start=min(self.data_start,segment.offset)
-        self.data_end=max(self.data_end,segment.offset+segment.length)
-
-
-segList = []
-session_list = {}
 
 ofh = open(PLOTFILE,"w")
 ifh = open(INFILE)
@@ -85,25 +138,46 @@ for segment in segList:
             session_list[currSession]=SessionData(segment)
         else:
             session_list[currSession].update(segment)
-        if segment.type < 8 :
-            data=segment.offset+segment.length
-            print("darrow",currTime,segment.offset,file=ofh)
-            print("rtext",currTime,segment.offset,file=ofh)
-            print("_",currSession,file=ofh)
-            print("uarrow",currTime,segment.offset+segment.length,file=ofh)
-            print("line",currTime,segment.offset,currTime,segment.offset+segment.length,file=ofh)
-        elif segment.type == 8 :
-            print("Report segment at",currTime,",",segment.rptCount,"claims")
-            for i in range(0,segment.rptCount) :
-                print("dline",currTime,segment.rptOffset[i],currTime,segment.rptOffset[i]+segment.rptLength[i],file=ofh)
-                print("rtext",currTime,segment.rptOffset[i],file=ofh)
-                print("_",currSession,file=ofh)
-print("=== Session report ===")
-print("Session, Segments, Start, End, Data(kB), Rate")
+    else:
+        MSGSTATUS[0] = True
+
+#print status/error messages
+for i in range(len(MSGLIST)):
+    if MSGSTATUS[i]:
+        print(MSGLIST[i])
+
+session_index = []
 for s in session_list.keys():
+    session_index.append((s,session_list[s].time_min))
+session_sorted = sorted(session_index, key=itemgetter(1))
+
+print("=== Session report ===")
+print("Session, Segments, Start, End, End-of-Data, Data(kB), Rate")
+
+yshift = session_list[session_sorted[0][0]].data_start
+for index in session_sorted:
+    s = index[0]
     sdata=(session_list[s].data_end-session_list[s].data_start)/1000
     print(f'{s:8}',f'{session_list[s].count:6}',f'{(session_list[s].time_min-time0):8.4f}', \
-          f'{(session_list[s].time_max-time0):8.4f}', \
-          sdata,f'{(8*sdata/(session_list[s].time_max-session_list[s].time_min)/1000):8.3f}Mbps')
+          f'{(session_list[s].time_max-time0):8.4f}', f'{(session_list[s].dtime_max-time0):8.4f}', \
+          sdata,f'{(8*sdata/(session_list[s].dtime_max-session_list[s].dtime_min)/1000):8.3f}Mbps')
+    for segment in session_list[s].segments:
+        currTime=segment.time-time0
+        currSession=segment.session
+        if segment.type < 8 :
+            print("darrow",currTime,segment.offset+yshift,file=ofh)
+            print("rtext",currTime,segment.offset+yshift,file=ofh)
+            print("_",currSession,file=ofh)
+            print("uarrow",currTime,segment.offset+segment.length+yshift,file=ofh)
+            print("line",currTime,segment.offset+yshift,currTime,segment.offset+segment.length+yshift,file=ofh)
+        elif segment.type == 8 :
+#            print("Report segment at",currTime,",",segment.rptCount,"claims")
+            for i in range(0,segment.rptCount) :
+                print("dline",currTime,segment.rptOffset[i]+yshift,currTime,segment.rptOffset[i]+segment.rptLength[i]+yshift,file=ofh)
+                print("rtext",currTime,segment.rptOffset[i]+yshift,file=ofh)
+                print("_",currSession,file=ofh)
+    yshift += session_list[s].data_end
+
 ifh.close()
 ofh.close()
+       
